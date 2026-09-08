79b42164bd67544c33a266a3d987152a4de7a723
SanjeevAmbalam15 <67994983+SanjeevAmbalam@users.noreply.github.com>
2026-09-07 19:52:50 -0700

    Add: number-of-students-unable-to-eat-lunch - submission-4

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-4.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, val):
+        self.next = None
+        self.val = val
+
+class Solution:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        newNode = Node(-1)
+        self.head = self.tail = newNode
+        remaining_student_count = len(students)
+        sandwiches_index = 0
+        for i in range(len(students)):
+            newNode = Node(students[i])
+            self.tail.next = newNode
+            self.tail = self.tail.next
+        
+        self.head = self.head.next
+        number_of_rotations = 0
+        while True:
+            # modified sandwiches
+            if len(sandwiches) == 0: return 0
+            #if self.blocked(sandwiches[0]):
+            if number_of_rotations == remaining_student_count:
+                return remaining_student_count
+            if sandwiches[sandwiches_index] == self.head.val:
+                sandwiches_index += 1
+                number_of_rotations = 0
+                self.head = self.head.next
+                remaining_student_count -= 1
+            else:
+                self.tail.next = self.head
+                self.tail = self.tail.next
+                self.head = self.head.next
+                self.tail.next = None
+                number_of_rotations += 1
+
+
